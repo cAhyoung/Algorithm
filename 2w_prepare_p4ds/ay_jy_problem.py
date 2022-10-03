78f8b4aa2b0c61d1620070a19a54d17c956ea934
Ahyoung Cho <93778822+cAhyoung@users.noreply.github.com>
2022-10-03 20:41:56 +0900

    Create ay_jy_problem.py

---
+++ 2w_prepare_p4ds/ay_jy_problem.py
@@ -0,0 +1,13 @@
+# 1 importing numpy
+import numpy as np
+
+# 2 printing numpy version
+print(np.__version__)
+
+# 3 making zero matrix
+zeros_array = np.zeros((10))
+print(zeros_array)
+
+# 4 changing 5th factor to 1
+zeros_array[4] = 1
+print(zeros_array)
