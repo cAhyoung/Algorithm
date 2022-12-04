9e42e6149009acb1811fee515a191354b904f4fe
Ahyoung Cho <93778822+cAhyoung@users.noreply.github.com>
2022-12-04 16:35:37 +0900

    Update ay_sliding_window_1.py

--- last_week/ay_sliding_window_1.py
+++ last_week/ay_sliding_window_1.py
@@ -1,1 +1,65 @@
+checkList = [0]*4
+myList = [0]*4
+checkSecret = 0
 
+def myadd(c): # 입력받은 리스트와 확인하기 위해 checkList를 이용해 확인함
+    global checkList, mylist, checkSecret
+    if c == 'A':
+        myList[0] += 1
+        if myList[0] == checkList[0]:
+            checkSecret += 1
+    elif c == 'C':
+        myList[1] += 1
+        if myList[1] == checkList[1]:
+            checkSecret += 1
+    elif c == 'G':
+        myList[2] += 1
+        if myList[2] == checkList[2]:
+            checkSecret += 1
+    elif c == 'T':
+        myList[3] += 1
+        if myList[3] == checkList[3]:
+            checkSecret += 1
+
+def myremove(c):  # 확인 후 리스트의 내용물들을 삭제
+    global checkList, myList, checkSecret
+    if c == 'A':
+        if myList[0] == checkList[0]:
+            checkSecret -= 1
+        myList[0] -= 1
+    elif c == 'C':
+        if myList[1] == checkList[1]:
+            checkSecret -= 1
+        myList[1] -= 1
+    elif c == 'G':
+        if myList[2] == checkList[2]:
+            checkSecret -= 1
+        myList[2] -= 1
+    elif c == 'T':
+        if myList[3] == checkList[3]:
+            checkSecret -= 1
+        myList[3] -= 1
+
+S, P = map(int, input("total long, part long: ").split())
+A = list(input('DNA: '))
+checkList = list(map(int, input('A, C, G, T: ').split()))
+Result = 0
+
+for i in range(4):
+    if checkList[i] == 0:
+        checkList += 1
+
+for i in range(P):
+    myadd(A[i])
+
+if checkSecret == 4:
+    Result += 1
+
+for i in range(P, S):
+    j = i - P
+    myadd(A[i])
+    myremove(A[j])
+    if checkSecret == 4:
+        Result += 1
+
+print(Result)
